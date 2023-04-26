0466de78d2e71671715e3f9d5cbe01115679e6c2
jr <alu0101471846@ull.edu.es>
2023-04-26 20:43:15 +0100

    Menú, progresos en funcionamiento. Bugfixes varios.

--- Codigo/build/CMakeFiles/fischillo.dir/DependInfo.cmake
+++ Codigo/build/CMakeFiles/fischillo.dir/DependInfo.cmake
@@ -1,18 +1,20 @@
-
-# Consider dependencies only in project.
-set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
-
 # The set of languages for which implicit dependencies are needed:
 set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
   )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jr/Desktop/SmartLock/Codigo/src/base_datos.cc" "/home/jr/Desktop/SmartLock/Codigo/build/CMakeFiles/fischillo.dir/src/base_datos.cc.o"
+  "/home/jr/Desktop/SmartLock/Codigo/src/cerradura_inteligente.cc" "/home/jr/Desktop/SmartLock/Codigo/build/CMakeFiles/fischillo.dir/src/cerradura_inteligente.cc.o"
+  "/home/jr/Desktop/SmartLock/Codigo/src/cliente.cc" "/home/jr/Desktop/SmartLock/Codigo/build/CMakeFiles/fischillo.dir/src/cliente.cc.o"
+  "/home/jr/Desktop/SmartLock/Codigo/src/cliente_funciones.cc" "/home/jr/Desktop/SmartLock/Codigo/build/CMakeFiles/fischillo.dir/src/cliente_funciones.cc.o"
+  "/home/jr/Desktop/SmartLock/Codigo/src/usuario.cc" "/home/jr/Desktop/SmartLock/Codigo/build/CMakeFiles/fischillo.dir/src/usuario.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
-# The set of dependency files which are needed:
-set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/hugohdezm2003/Trabajo_Grupal_FIS/Codigo/src/base_datos.cc" "CMakeFiles/fischillo.dir/src/base_datos.cc.o" "gcc" "CMakeFiles/fischillo.dir/src/base_datos.cc.o.d"
-  "/home/hugohdezm2003/Trabajo_Grupal_FIS/Codigo/src/cerradura_inteligente.cc" "CMakeFiles/fischillo.dir/src/cerradura_inteligente.cc.o" "gcc" "CMakeFiles/fischillo.dir/src/cerradura_inteligente.cc.o.d"
-  "/home/hugohdezm2003/Trabajo_Grupal_FIS/Codigo/src/cliente.cc" "CMakeFiles/fischillo.dir/src/cliente.cc.o" "gcc" "CMakeFiles/fischillo.dir/src/cliente.cc.o.d"
-  "/home/hugohdezm2003/Trabajo_Grupal_FIS/Codigo/src/cliente_funciones.cc" "CMakeFiles/fischillo.dir/src/cliente_funciones.cc.o" "gcc" "CMakeFiles/fischillo.dir/src/cliente_funciones.cc.o.d"
-  "/home/hugohdezm2003/Trabajo_Grupal_FIS/Codigo/src/usuario.cc" "CMakeFiles/fischillo.dir/src/usuario.cc.o" "gcc" "CMakeFiles/fischillo.dir/src/usuario.cc.o.d"
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
   )
 
 # Targets to which this target links.
